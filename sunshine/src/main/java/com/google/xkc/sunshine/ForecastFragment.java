7c6bd7237c88adfb35178682ce7aeb7ffa5dd017
xkc <xkc@xkc.cn>
2016-01-08 21:53:47 +0800

    点击Refresh菜单选项，通过AsyncTask进行异步网络操作，获取OpenWeatherMap返回的天气数据，并显示在ListView中

--- sunshine/src/main/java/com/google/xkc/sunshine/ForecastFragment.java
+++ sunshine/src/main/java/com/google/xkc/sunshine/ForecastFragment.java
@@ -1,13 +1,21 @@
 package com.google.xkc.sunshine;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -42,89 +50,185 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //to report this fragment has menu options
+        //add this line in order for this fragment to handle options menu events
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
 
+        initView(rootView);
+
+        initData();
+
+        return rootView;
+    }
+
+    private void initView(View rootView) {
+        listview_forecast = (ListView) rootView.findViewById(R.id.listview_forecast);
+    }
+
+    private void initData() {
         List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+
 
         mForecastAapter = new ArrayAdapter<>(getActivity(),
                 R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
 
 
-        listview_forecast = (ListView) rootView.findViewById(R.id.listview_forecast);
+
         listview_forecast.setAdapter(mForecastAapter);
-
-        getForecastStr();
-
-
-        return rootView;
     }
 
-    private String getForecastStr() {
-        // These two need to be declared outside the try/catch
-        // so that they can be closed in the finally block.
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
 
-        // Will contain the raw JSON response as a string.
-        String forecastJsonStr = null;
+    class FetchWeatherTask extends AsyncTask<String, Void, String> {
 
-        try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are avaiable at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily" +
-                    "?q=94043&mode=json&units=metric&cnt=7&APPID=b49b573ea5b0e5150417928348d05344");
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
-            // Create the request to OpenWeatherMap, and open the connection
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
+        @Override
+        protected String doInBackground(String... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
 
-            // Read the input stream into a String
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuilder builder = new StringBuilder();
-            if (inputStream == null){
-                // Nothing to do.
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+
+            String query = params[0];
+            String format = "json";
+            String units = "metric";
+            int numDays = 7;
+            String appid = "b49b573ea5b0e5150417928348d05344";
+
+
+
+
+            try {
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+                final String APPID_PARAM = "APPID";
+
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, query)
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .appendQueryParameter(APPID_PARAM, appid)
+                        .build();
+//                Log.i(LOG_TAG,builtUri.toString());
+//
+//                StringBuilder urlBuilder = new StringBuilder(FORECAST_BASE_URL);
+//                urlBuilder.append("?q="+params[0]);
+//                urlBuilder.append("&mode=json&units=metric&cnt=7&APPID=b49b573ea5b0e5150417928348d05344");
+//                Log.i(LOG_TAG,urlBuilder.toString());
+
+
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuilder builder = new StringBuilder();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    builder.append(line + "\n");
+                }
+
+                if (builder.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+
+                forecastJsonStr = builder.toString();
+
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
                 return null;
-            }
-
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null){
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                builder.append(line + "\n");
-            }
-
-            if (builder.length() == 0 ){
-                // Stream was empty.  No point in parsing.
-                return null;
-            }
-
-            forecastJsonStr = builder.toString();
-
-
-        }  catch (IOException e) {
-            Log.e("ForecastFragment","Error",e);
-            return null;
-        }finally {
-            if (urlConnection != null){
-                urlConnection.disconnect();
-            }
-            if (reader != null){
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    Log.e("MainActivityFramgment","Error closing stream",e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
                 }
             }
+            Log.i(LOG_TAG, "forecast jsonStr=" + forecastJsonStr);
+            return forecastJsonStr;
         }
 
-        return forecastJsonStr;
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_forecast_fragment, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_refresh:
+//                Toast.makeText(getActivity(),"refresh",Toast.LENGTH_LONG).show();
+                new FetchWeatherTask(){
+                    @Override
+                    protected void onPostExecute(String s) {
+                        Log.i("xkc","s = "+s);
+                        try {
+                            String[] resultStr = WeatherDataHelper.getWeatherDataFromJson(s,7);
+                            List<String> forecastList = Arrays.asList(resultStr);
+                            ArrayAdapter adapter = new ArrayAdapter(getActivity(),
+                                    R.layout.list_item_forecast, R.id.list_item_forecast_textview,
+                                    forecastList);
+                            listview_forecast.setAdapter(adapter);
+                        } catch (JSONException e) {
+                           Log.e("xkc","json exception:"+e.getMessage());
+                        }
+
+                    }
+                }.execute("Beijing");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 }
