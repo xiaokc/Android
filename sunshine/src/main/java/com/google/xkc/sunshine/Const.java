02cdd2617491ef94388297866c63c49230196aaa
xkc <xkc@xkc.cn>
2016-01-14 10:25:58 +0800

    将openWeatherMap的APPID和中文提示语句放到Const中

---
+++ sunshine/src/main/java/com/google/xkc/sunshine/Const.java
@@ -0,0 +1,9 @@
+package com.google.xkc.sunshine;
+
+/**
+ * Created by xkc on 1/13/16.
+ */
+public class Const {
+    public static final String OPEN_WEATHER_MAP_API_KEY = "b49b573ea5b0e5150417928348d05344";
+    public static final String NO_NETWORK = "The Network Is Not Available!";
+}
