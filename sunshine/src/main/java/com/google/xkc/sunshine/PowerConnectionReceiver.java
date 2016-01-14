dfde92676e72105da2cab4b633468ae352bdcd68
xkc <xkc@xkc.cn>
2016-01-14 10:29:01 +0800

    通过BroadcastReceiver检测设备连接电源的情况，待完善

---
+++ sunshine/src/main/java/com/google/xkc/sunshine/PowerConnectionReceiver.java
@@ -0,0 +1,14 @@
+package com.google.xkc.sunshine;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by xkc on 1/13/16.
+ */
+public class PowerConnectionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}
