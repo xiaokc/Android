07713308a8281e45b81e362aa0df1abc4e89e655
xkc <xkc@xkc.cn>
2016-01-17 09:06:09 +0800

    save the const of theMovieDB API key

---
+++ mytheater/src/main/java/com/google/xkc/mytheater/Const.java
@@ -0,0 +1,8 @@
+package com.google.xkc.mytheater;
+
+/**
+ * Created by xkc on 1/14/16.
+ */
+public class Const {
+    public static final String THE_MOVIE_DB_APIKEY = "669e3ca840a4d95a84bd0adfe7d7bc63";
+}
